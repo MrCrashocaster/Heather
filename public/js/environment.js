802b1ab7ca7a217a322c11468cecad2a0130b6c5
MrCrashocaster <mrcrashocaster@gmail.com>
2023-03-18 20:13:08 -0500

    Updated site.

--- public/js/environment.js
+++ public/js/environment.js
@@ -7,13 +7,12 @@ import { getAnalytics } from "firebase/analytics";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyCtpWyRjPJpJl5wnVf9niJEOkBAHwbnhMA",
-  authDomain: "template-f3229.firebaseapp.com",
-  projectId: "template-f3229",
-  storageBucket: "template-f3229.appspot.com",
-  messagingSenderId: "58302981614",
-  appId: "1:58302981614:web:815cb16d0d2a0435a1daff",
-  measurementId: "G-VC7YB7F27P"
+  apiKey: "AIzaSyDJdsAbK4KMdeBgITSDuG1JlcD7_e82__0",
+  authDomain: "heather-harrell.firebaseapp.com",
+  projectId: "heather-harrell",
+  storageBucket: "heather-harrell.appspot.com",
+  messagingSenderId: "657591822480",
+  appId: "1:657591822480:web:c78d3b01e471b5add3d015"
 };
 
 // Initialize Firebase
