3366c58051d3201267acdba40104353e65b53c1f
MrCrashocaster <mrcrashocaster@gmail.com>
2023-03-18 20:56:15 -0500

    Updated site.

--- public/js/site.js
+++ public/js/site.js
@@ -1,4 +1,9 @@
-function navBarFunction() {
-    document.getElementsByClassName("navbar")[0].classList.toggle("responsive");
-    document.getElementsByClassName("div_navbar_items")[0].classList.toggle("fade-in");
-}
+$( document ).ready(function() {
+
+    $('html').addClass('ready');
+
+    function navBarFunction() {
+        document.getElementsByClassName("navbar")[0].classList.toggle("responsive");
+        document.getElementsByClassName("div_navbar_items")[0].classList.toggle("fade-in");
+    }
+});
