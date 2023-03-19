9aefd48f25dbcf1975140c8800e189bd61408e5e
MrCrashocaster <mrcrashocaster@gmail.com>
2023-03-18 21:38:33 -0500

    Updated site.

--- public/js/site.js
+++ public/js/site.js
@@ -6,4 +6,26 @@ $( document ).ready(function() {
         document.getElementsByClassName("navbar")[0].classList.toggle("responsive");
         document.getElementsByClassName("div_navbar_items")[0].classList.toggle("fade-in");
     }
+
+    $('.fade-up, .fade-down, .fade-right, .fade-left').each(function() {
+        var crrnt = $(this);
+        if (crrnt.visible( true )) {
+            crrnt.addClass('show');
+        }
+        else if (!crrnt.visible( true )) {
+            crrnt.removeClass('show');
+        }
+    });
+});
+
+$(window).on('scroll', function(){
+    $('.fade-up, .fade-down, .fade-right, .fade-left').each(function() {
+        var crrnt = $(this);
+        if (crrnt.visible( true )) {
+            crrnt.addClass('show');
+        }
+        else if (!crrnt.visible( true )) {
+            crrnt.removeClass('show');
+        }
+    });
 });
